7dd6fe2028bdf498454e30303e504a91b2c5a4f6
Eduardo Policarpo <30879448+edupoli@users.noreply.github.com>
2021-11-25 03:58:55 -0300

    empty file index

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 01:00:12                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-11-25 01:25:04                                           #
+# Last Modified: 2021-11-25 03:58:35                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
-var latest = require('github-latest-release')
-
-latest('eduardo-policarpo', 'wa-js')
-    .then(console.log.bind(console))
