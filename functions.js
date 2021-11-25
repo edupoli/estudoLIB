85e306adb9789283bfe17f2387b3901e57664403
Eduardo Policarpo <30879448+edupoli@users.noreply.github.com>
2021-11-25 03:30:43 -0300

    feat: add function sendbuttons
    adicionado para trabalhar em multidevice

---
+++ functions.js
@@ -0,0 +1,9 @@
+/*##############################################################################
+# File: functions.js                                                           #
+# Project: estudolib                                                           #
+# Created Date: 2021-11-25 03:28:14                                            #
+# Author: Eduardo Policarpo                                                    #
+# Last Modified: 2021-11-25 03:28:16                                           #
+# Modified By: Eduardo Policarpo                                               #
+##############################################################################*/
+
