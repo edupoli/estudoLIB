7d405ff17344497b769bf4822e5238b420634fa5
Eduardo Policarpo <30879448+edupoli@users.noreply.github.com>
2021-11-25 04:01:23 -0300

    add path in index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 01:00:12                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-11-25 03:58:35                                           #
+# Last Modified: 2021-11-25 04:01:06                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
+import path from 'path';
