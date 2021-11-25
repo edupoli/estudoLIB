966cb27971e969bf4116f4c4d8a6f9c5f3f19b20
Eduardo Policarpo <30879448+edupoli@users.noreply.github.com>
2021-11-25 04:03:54 -0300

    add fs in index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 01:00:12                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-11-25 04:01:06                                           #
+# Last Modified: 2021-11-25 04:03:39                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
 import path from 'path';
+import fs from 'fs';
