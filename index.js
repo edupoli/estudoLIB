666f9f9df7560feedff88c42100b578c65e106ad
edupoli <eduardopolicarpo@gmail.com>
2021-12-01 11:51:34 -0300

    fix: add comments in index file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 01:00:12                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-11-25 04:03:39                                           #
+# Last Modified: 2021-12-01 11:51:09                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
 import path from 'path';
-import fs from 'fs';
+import fs from 'fs';//
