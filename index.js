f4cc83c7cac5a0a9193c8e05183c0678d0e4a4d5
edupoli <eduardopolicarpo@gmail.com>
2021-12-01 12:01:15 -0300

    fix: remove comments

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 01:00:12                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-12-01 11:51:09                                           #
+# Last Modified: 2021-12-01 12:00:55                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
 import path from 'path';
-import fs from 'fs';//
+import fs from 'fs';
