676ad0e83a471e459d5022e46b511daad809cf68
edupoli <eduardopolicarpo@gmail.com>
2021-12-02 02:00:26 -0300

    KKK

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 01:00:12                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-12-01 12:05:38                                           #
+# Last Modified: 2021-12-02 02:00:16                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
 import path from 'path';
-import fs from 'fs';//
+import fs from 'fs';
