a62fb6228f74c36c3452e402ac21353ef1ec1289
edupoli <eduardopolicarpo@gmail.com>
2021-12-02 19:21:49 -0300

    yy

--- functions.js
+++ functions.js
@@ -3,8 +3,8 @@
 # Project: estudolib                                                           #
 # Created Date: 2021-11-25 03:28:14                                            #
 # Author: Eduardo Policarpo                                                    #
-# Last Modified: 2021-12-02 19:19:19                                           #
+# Last Modified: 2021-12-02 19:21:42                                           #
 # Modified By: Eduardo Policarpo                                               #
 ##############################################################################*/
 
-$
+$hh
